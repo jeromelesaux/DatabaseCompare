98fb499650f046f66bdaedce44c897d061f59f84
jlesaux <bzh210575>
2014-01-28 18:25:22 +0100

    add cmdline class

---
+++ jar/database-compare-tool/src/main/java/fr/axione/dbcompare/Tool/CmdLineTool.java
@@ -0,0 +1,16 @@
+package fr.axione.dbcompare.Tool;
+
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+/**
+ * Created by jlesaux on 28/01/14.
+ */
+public class CmdLineTool {
+
+    public static void main(String[] args) {
+        OptionParser parser = new OptionParser("");
+        OptionSet options = parser.parse("");
+    }
+}
